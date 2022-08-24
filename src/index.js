1a84f32429efd5d1dd0a8f4d4c93c4b2072077dc
Ninad Kulkarni <ninadk03@gmail.com>
2022-08-24 15:22:01 -0700

    Added bootstrap import to project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// Bootstrap CSS and JS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// Global project CSS. Use App.css for page level css.
+import './css/index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App/>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
