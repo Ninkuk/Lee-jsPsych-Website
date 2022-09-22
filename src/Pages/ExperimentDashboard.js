408bbb589c5f88c179818594269e670bee7b66c0
ninkuk <ninadk03@gmail.com>
2022-09-22 08:22:00 -0700

    Added a blank Experiment Dashboard

---
+++ src/Pages/ExperimentDashboard.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import {useLocation, useParams} from "react-router-dom";
+
+function ExperimentDashboard(props) {
+	const data = useLocation();
+
+	return (
+		<div>Experiment Dashboard</div>
+	);
+}
+
+export default ExperimentDashboard;
