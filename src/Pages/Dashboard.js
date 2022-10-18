df5223b2a314a4f3bd33769f5938d257821a417e
ninkuk <ninadk03@gmail.com>
2022-10-18 14:25:25 -0700

    added filters to search

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -51,13 +51,45 @@ function Dashboard(props) {
 					</ul>
 				</div>
 			</div>
-			<div className="input-group mb-3">
-				<span className="input-group-text bg-transparent border-end-0"><i className="bi bi-search opacity-75"/></span>
-				<input type="text" className="form-control fs-6 bg-transparent border-start-0" placeholder="Search"/>
+
+			{/* Filters */}
+			<div className="row g-2">
+				<div className="input-group mb-3 col">
+						<span className="input-group-text bg-transparent border-end-0"><i
+							className="bi bi-search opacity-75"/></span>
+					<input type="text" className="form-control fs-6 bg-transparent border-start-0"
+					       placeholder="Search"/>
+				</div>
+				<div className="dropdown col-auto">
+					<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+						<i className="bi bi-funnel-fill"></i> Sort
+					</button>
+					<div className="dropdown-menu dropdown-menu-dark dropdown-menu-end px-2">
+						<div className="form-check">
+							<input className="form-check-input" type="checkbox" value="s2MC" id="s2MC"/>
+							<label className="form-check-label" htmlFor="s2MC">
+								Stimuli to Multiple Choice
+							</label>
+						</div>
+						<div className="form-check">
+							<input className="form-check-input" type="checkbox" value="s2MC" id="s2N" checked/>
+							<label className="form-check-label" htmlFor="s2N">
+								Stimuli to Numerical
+							</label>
+						</div>
+						<div className="form-check">
+							<input className="form-check-input" type="checkbox" value="s2A" id="s2A" checked/>
+							<label className="form-check-label" htmlFor="s2MC">
+								Stimuli to Alphabet
+							</label>
+						</div>
+					</div>
+				</div>
 			</div>
 
+
 			{/* Cards */}
-			<div className="experiment-cards mt-4">
+			<div className="experiment-cards">
 				<div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
 					{
 						userExperiments.map(experiment => (
